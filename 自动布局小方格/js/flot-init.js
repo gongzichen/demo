fcc6474bbe2cde86a1c1c85ef45c0c0fbb94d948
gongzichen <12639179@qq.com>
2017-03-09 23:02:04 +0800

    作品

---
+++ 自动布局小方格/js/flot-init.js
@@ -0,0 +1,123 @@
+﻿/**
+ * 获取style这个类名对应的color值
+ * @param style 类名
+ * @returns color颜色值
+ */
+function getThemeColorFromCss(style) {
+    // 往body里面添加了一个隐藏的span
+    var $span = $("<span></span>").hide().appendTo("body");
+    // 给span添加类名
+    $span.addClass(style);
+    // 获取span的color值
+    var color = $span.css("color");
+    // 移除span
+    $span.remove();
+    // 返回color值
+    return color;
+}
+
+var themeprimary = getThemeColorFromCss('themeprimary');
+var themesecondary = getThemeColorFromCss('themesecondary');
+var themethirdcolor = getThemeColorFromCss('themethirdcolor');
+
+var InitiateFlotBarChart = function () {
+    return {
+        init: function () {
+            var data2 = [{
+                color: themesecondary,
+                label: "shuju1",
+                data: [[3, 2], [4, 5], [5, 4], [6, 11], [7, 12], [8, 11], [9, 8], [10, 14], [11, 12], [12, 16], [13, 9],
+                    [14, 10], [15, 14], [16, 15], [17, 9]],
+
+                lines: {
+                    show: true,
+                    fill: true,
+                    lineWidth: .1,
+                    fillColor: {
+                        colors: [{
+                            opacity: 0
+                        }, {
+                            opacity: 0.4
+                        }]
+                    }
+                },
+                points: {
+                    show: false
+                },
+                shadowSize: 0
+            },
+                {
+                    color: themeprimary,
+                    label: "shuju2",
+                    data: [[3, 10], [4, 13], [5, 12], [6, 16], [7, 19], [8, 19], [9, 24], [10, 19], [11, 18], [12, 21], [13, 17],
+                        [14, 14], [15, 12], [16, 14], [17, 15]],
+                    bars: {
+                        order: 1,
+                        show: true,
+                        borderWidth: 0,
+                        barWidth: 0.4,
+                        lineWidth: .5,
+                        fillColor: {
+                            colors: [{
+                                opacity: 0.4
+                            }, {
+                                opacity: 1
+                            }]
+                        }
+                    }
+                },
+                {
+                    color: themethirdcolor,
+                    label: "shuju3",
+                    data: [[3, 14], [4, 11], [5, 10], [6, 9], [7, 5], [8, 8], [9, 5], [10, 6], [11, 4], [12, 7], [13, 4],
+                        [14, 3], [15, 4], [16, 6], [17, 4]],
+                    lines: {
+                        show: true,
+                        fill: false,
+                        fillColor: {
+                            colors: [{
+                                opacity: 0.3
+                            }, {
+                                opacity: 0
+                            }]
+                        }
+                    },
+                    points: {
+                        show: true
+                    }
+                }
+            ];
+            var options = {
+                legend: {
+                    show: false
+                },
+                xaxis: {
+                    tickDecimals: 0,
+                    color: '#f3f3f3'
+                },
+                yaxis: {
+                    min: 0,
+                    color: '#f3f3f3',
+                    tickFormatter: function (val, axis) {
+                        return "";
+                    },
+                },
+                grid: {
+                    hoverable: true,
+                    clickable: false,
+                    borderWidth: 0,
+                    aboveData: false,
+                    color: '#fbfbfb'
+
+                },
+                tooltip: true,
+                tooltipOpts: {
+                    defaultTheme: false,
+                    content: " <b>%x May</b> , <b>%s</b> : <span>%y</span>",
+                }
+            };
+            var placeholder = $("#bar-chart");
+            var plot = $.plot(placeholder, data2, options);
+        }
+    };
+}();
