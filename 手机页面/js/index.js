1dd33802cbae1665357cfb5db1bb04a180d52e19
gongzichen <12639179@qq.com>
2017-03-10 23:23:57 +0800

    更新手机专题

--- 手机页面/js/index.js
+++ 手机页面/js/index.js
@@ -9,24 +9,28 @@ $(function(){
         $('.bg').addClass('cur');
     });
 
-    //第二屏
-
+    //跳转第二屏
    $('.con-go').click(function(){
        $('#boxes').css('top',-boxH * 2);
-
-
    });
+        var config = {
+            reset:true,
+            origin:'bottom',
+            duration:500,
+            delay:0,
+            opacity: 0, // 初始透明度
+            scale: 0.5 //缩放
+        };
+        window.sr = ScrollReveal(config);
+        sr.reveal('.sr',300);
 
-    //function test(){
-    //    var config = {
-    //        reset:true,
-    //        origin:'bottom',
-    //        duration:500,
-    //        delay:0,
-    //        opacity: 0, // 初始透明度
-    //        scale: 0.5 //缩放
-    //    };
-    //    window.sr = ScrollReveal(config);
-    //    sr.reveal('.sr',100);
-    //}
+    // 跳转第三屏
+    $('.box2-con-go').click(function(){
+        $('#boxes').css('top',-boxH * 3);
+    });
+
+
+
+    //第三屏
+
 });
